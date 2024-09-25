7683b2aded9d86d02c63d040048c3d3d020b30f2
Ism1tha <xgalan@hotmail.com>
2024-09-25 19:09:48 +0200

    Refactor classes, modal javascript and fixes

--- ex3/assets/js/base.js
+++ ex3/assets/js/base.js
@@ -62,6 +62,7 @@ async function fetchPokemons() {
       pokemon.image = pokemonData.image;
       pokemon.sound = pokemonData.sound;
       pokemon.types = pokemonData.types;
+      pokemon.stats = pokemonData.stats;
     }
     console.log("Pokemons fetched", pokemonList);
     return true;
@@ -79,7 +80,9 @@ async function fetchPokemonBaseData(url) {
       image: data.sprites.front_default,
       sound: data.cries.legacy,
       types: data.types.map((item) => item.type.name),
+      stats: data.stats,
     };
+    console.log("Pokemon data fetched", pokemonData);
     return pokemonData;
   } catch (error) {
     console.log(error);
@@ -126,13 +129,13 @@ function initializePokemonsTable() {
       {
         targets: 1,
         render: function (data, type, row, meta) {
-          return row.types.map((type) => `<span class="badge ${getBadgeClass(type)}">${capitalizeFirstLetter(type)}</span>`).join(" ");
+          return row.types.map((type) => `<span class="badge ${getTypeClass(type)}">${capitalizeFirstLetter(type)}</span>`).join(" ");
         }
       },
       {
         targets: 2,
         render: function (data, type, row, meta) {
-          return `<button class="btn btn-third" onclick="playPokemonSound('${row.sound}')">
+          return `<button class="btn btn-third" onclick="playSound('${row.sound}')">
             <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><g fill="#ffffff"><path d="M10 16v-5.401A2.999 2.999 0 0 0 10 5.4V0L4.667 4H0v8h4.667z"/><path d="m13.6 3.2l-.799-.6L11.6 4.199l.8.6a4 4 0 0 1 .8.802c.503.668.8 1.497.8 2.399s-.297 1.73-.8 2.4a4 4 0 0 1-.8.8l-.8.601l1.201 1.6l.8-.601a6 6 0 0 0 1.198-1.2A5.98 5.98 0 0 0 16 8c0-1.35-.447-2.598-1.2-3.6a6 6 0 0 0-1.2-1.2"/></g></svg>
           </button>`;
         },
@@ -140,7 +143,7 @@ function initializePokemonsTable() {
       {
         targets: 3,
         render: function (data, type, row, meta) {
-          return `<button class="btn btn-primary" onclick="showPokemonDetails('${row.url}')"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><g fill="#ffffff"><path d="M8 10a2 2 0 1 0 0-4a2 2 0 0 0 0 4"/><path d="m15.356 7.478l.027.051l.235.471l-.236.47l-.026.052a13 13 0 0 1-.464.794a14 14 0 0 1-1.399 1.853C12.303 12.492 10.427 14 8 14s-4.302-1.508-5.493-2.831A14 14 0 0 1 .644 8.522l-.027-.051L.382 8l.235-.47a6 6 0 0 1 .125-.232a14 14 0 0 1 1.765-2.467C3.697 3.508 5.573 2 8 2s4.302 1.508 5.493 2.831a14 14 0 0 1 1.863 2.647m-12.558.768c.276.436.68 1.013 1.195 1.585C5.053 11.008 6.427 12 8 12s2.948-.992 4.007-2.169A12 12 0 0 0 13.354 8a12 12 0 0 0-1.347-1.831C10.947 4.992 9.573 4 8 4s-2.948.992-4.007 2.169A12 12 0 0 0 2.646 8q.068.113.152.246"/></g></svg></button> <button class="btn btn-secondary" onclick="deletePokemonFromData('${meta.row}')"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><g fill="#ffffff"><path d="M7 7v5h2V7z"/><path d="M11 0H5v3H0v2h1.165l1.553 8.537A3 3 0 0 0 5.669 16h4.662a3 3 0 0 0 2.952-2.463L14.835 5H16V3h-5zM9 3H7V2h2zM4.685 13.179L3.198 5h9.604l-1.487 8.179a1 1 0 0 1-.984.821H5.669a1 1 0 0 1-.984-.821"/></g></svg></button>`;
+          return `<button class="btn btn-primary" onclick="showPokemonDetails('${meta.row}')"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><g fill="#ffffff"><path d="M8 10a2 2 0 1 0 0-4a2 2 0 0 0 0 4"/><path d="m15.356 7.478l.027.051l.235.471l-.236.47l-.026.052a13 13 0 0 1-.464.794a14 14 0 0 1-1.399 1.853C12.303 12.492 10.427 14 8 14s-4.302-1.508-5.493-2.831A14 14 0 0 1 .644 8.522l-.027-.051L.382 8l.235-.47a6 6 0 0 1 .125-.232a14 14 0 0 1 1.765-2.467C3.697 3.508 5.573 2 8 2s4.302 1.508 5.493 2.831a14 14 0 0 1 1.863 2.647m-12.558.768c.276.436.68 1.013 1.195 1.585C5.053 11.008 6.427 12 8 12s2.948-.992 4.007-2.169A12 12 0 0 0 13.354 8a12 12 0 0 0-1.347-1.831C10.947 4.992 9.573 4 8 4s-2.948.992-4.007 2.169A12 12 0 0 0 2.646 8q.068.113.152.246"/></g></svg></button> <button class="btn btn-secondary" onclick="deletePokemonFromData('${meta.row}')"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><g fill="#ffffff"><path d="M7 7v5h2V7z"/><path d="M11 0H5v3H0v2h1.165l1.553 8.537A3 3 0 0 0 5.669 16h4.662a3 3 0 0 0 2.952-2.463L14.835 5H16V3h-5zM9 3H7V2h2zM4.685 13.179L3.198 5h9.604l-1.487 8.179a1 1 0 0 1-.984.821H5.669a1 1 0 0 1-.984-.821"/></g></svg></button>`;
         },
       },
     ],
@@ -148,9 +151,6 @@ function initializePokemonsTable() {
       search: "",
       searchPlaceholder: 'Pikachu, Bulbasaur, Charmander...',
     },
-    headerCallback: function(thead) {
-      //$(thead).hide();  // Hides the table header
-    }
   });
 }
 
@@ -163,8 +163,8 @@ function extractPokemonId(url) {
   return splitUrl[splitUrl.length - 2];
 }
 
-function getBadgeClass(type) {
-  return `badge-${type.replace(" ", "-").toLowerCase()}`;
+function getTypeClass(type) {
+  return `${type.replace(" ", "-").toLowerCase()}`;
 }
 
 /* Data Storage Functions */
@@ -294,6 +294,8 @@ function showToast(message) {
 
 function showPokemonDetails(index) {
   playSound("assets/sounds/pc-effect.mp3");
+  console.log(index);
+  updatePokemonDetails(index);
   document.getElementById("pokemon-modal-overlay").style.display = "block";
   showingPokemonDetails = true;
 }
@@ -301,6 +303,31 @@ function showPokemonDetails(index) {
 function closePokemonDetails() {
   document.getElementById("pokemon-modal-overlay").style.display = "none";
   showingPokemonDetails = false;
+}
+
+function updatePokemonDetails(index) {
+  var pokemon = pokemonList[index];
+  var pokemonBackgroundElement = document.getElementById("pokemon-background");
+  var pokemonNameElement = document.getElementById("pokemon-name");
+  var pokemonImageElement = document.getElementById("pokemon-image");
+  var pokemonTypesElement = document.getElementById("pokemon-types");
+  var pokemonStatsHP = document.getElementById("pokemon-hp");
+  var pokemonStatsAttack = document.getElementById("pokemon-attack");
+  var pokemonStatsDefense = document.getElementById("pokemon-defense");
+  var pokemonStatsSpecialAttack = document.getElementById("pokemon-special-attack");
+  var pokemonStatsSpecialDefense = document.getElementById("pokemon-special-defense");
+  var pokemonStatsSpeed = document.getElementById("pokemon-speed");
+
+  pokemonBackgroundElement.className = getTypeClass(pokemon.types[0]);
+  pokemonNameElement.innerHTML = capitalizeFirstLetter(pokemon.name);
+  pokemonImageElement.src = pokemon.image;
+  pokemonTypesElement.innerHTML = pokemon.types.map((type) => `<span class="badge ${getTypeClass(type)}">${capitalizeFirstLetter(type)}</span>`).join(" ");
+  pokemonStatsHP.innerHTML = pokemon.stats[0].base_stat;
+  pokemonStatsAttack.innerHTML = pokemon.stats[1].base_stat;
+  pokemonStatsDefense.innerHTML = pokemon.stats[2].base_stat;
+  pokemonStatsSpecialAttack.innerHTML = pokemon.stats[3].base_stat;
+  pokemonStatsSpecialDefense.innerHTML = pokemon.stats[4].base_stat;
+  pokemonStatsSpeed.innerHTML = pokemon.stats[5].base_stat;
 }
 
 /* Set timeout, to simulate a delay 3 seconds */
