d7c031081fd0c47922b7b5f2b24459415907bf99
Ism1tha <xgalan@hotmail.com>
2024-10-16 09:37:35 +0200

    Base structure and files for pt4 (Invoices)

---
+++ ex4/assets/js/base.js
@@ -0,0 +1,23 @@
+/**
+ * Exercici 3 - Pokedex amb API / INS Montsià MP06
+ * Author: Ismael Semmar Galvez
+ *
+ */
+
+/* Constants */
+
+/* Variables */
+
+/* Application Functions */
+
+/* Data Storage Functions */
+
+/* UX Functions */
+
+/* Set timeout, to simulate a delay 3 seconds */
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(() => {
+    console.log("DOM fully loaded and parsed");
+  }, 3000);
+});
